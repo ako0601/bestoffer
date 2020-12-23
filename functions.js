1722c3977a5b73cc9733605ad21374491d90cfed
Kwangok Ahn <dksrhkddhr06@gmail.com>
2020-12-24 08:31:31 +0900

    New Commit

---
+++ functions.js
@@ -0,0 +1,11 @@
+function bid() {
+    var nextBid = 1000;
+    var userID = 111;
+
+    if(userID == 111) {
+        alert("입찰에 성공했습니다.")
+        nextBid++1000;
+    } else {
+        alert("입찰에 실패했습니다.")
+    }
+}
